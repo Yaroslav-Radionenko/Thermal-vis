e15081059e39744fce96c3405b13669794b02a0b
Yaroslav-Radionenko <radionenkoyaroslav.stud@gmail.com>
2025-06-27 23:16:14 +0200

    Comments added
    Added comments for the code in english

--- Thermal_cam/Thermal_cam.ino
+++ Thermal_cam/Thermal_cam.ino
@@ -4,13 +4,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 
-// Кнопки керування та індикація
+// Control buttons and indicator
 const int btn_ok = 2;
 const int btn_left = 3;
 const int btn_right = 4;
 const int led = 5;
 
-// SPI інтерфейс
+// SPI interface
 const int tft_rst = 8;
 const int tft_dc = 9;
 const int tft_cs = 10;
@@ -18,10 +18,10 @@ const int tft_cs = 10;
 const int tft_sda = 11;
 const int tft_scl = 13;
 
-// До сенсора за виключенням I2C
+// To the sensor, except for I2C
 const int tft_int = 12;
 
-// Масив пікселей сенсора
+// Sensor pixel array
 const int gridSize = 8;
 const int boxSize = 16;
 float pixels[64];
@@ -36,14 +36,14 @@ float tMax = 30;
 float filtredMinT = tMin;
 float filtredMaxT = tMax;
 
-// Створення обʼєкта сенсору 
+// Create sensor object
 Adafruit_AMG88xx amg;
 
-// Створення обʼєкта дисплею
+// Create display object
 Adafruit_ST7735 tft = Adafruit_ST7735(tft_cs, tft_dc, tft_rst);
 const int usableHeight = 160 - 32;
 
-// Затримка
+// Delay
 unsigned long lastUpdate = 0;
 const unsigned long updateInterval = 100;
 unsigned long lastUpdateTime = 0;
@@ -53,14 +53,14 @@ unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 unsigned long del_menu = 0;
 
-// Екран
+// Screen
 int menuIndex = 0;
 int submenuIndex = 0;
 
 bool menuVisible = false;
 bool inSubmenu = false;
 
-// Режими екранів
+// Screen modes
 const char* mainMenu[] = {"Palette", "Text Temp." , "Pause"};
 const int mainMenuSize = sizeof(mainMenu)/ sizeof(mainMenu[0]);
 
@@ -76,7 +76,7 @@ int currentTempMode = 0;
 
 bool isPaused = false;
 
-// Кнопки
+// Buttons
 bool btnPressed_ok = true;
 bool btnPressed_left = true;
 bool btnPressed_right = true;
@@ -87,7 +87,7 @@ int lastButtonState_right = HIGH;
 int buttonState;
 
 void setup() {
-  // Підготовка кнопок
+  // Prepare buttons
   pinMode (btn_ok, INPUT);
   pinMode (btn_left, INPUT);
   pinMode (btn_right, INPUT);
@@ -95,7 +95,7 @@ void setup() {
 
   digitalWrite (led, LOW);
 
-  // Початкове налаштування дисплею
+  // Initialize display
   tft.initR(INITR_BLACKTAB);
   tft.setRotation(2);
   tft.fillScreen(ST7735_BLACK);
@@ -104,7 +104,7 @@ void setup() {
   tft.setCursor(24, 30);
   tft.println("Welcome");
 
-  // Перевірка наявності сенсора
+  // Check if sensor is connected
   if (!amg.begin(0x68)) {
     tft.fillScreen(ST7735_BLACK);
     tft.setTextColor(ST7735_RED);
@@ -113,17 +113,19 @@ void setup() {
     tft.println("AMG8833 not found!");
     while (true);
   }
-  delay (2000);
+  delay (2000);                                         // Delay before program start
 }
 
 void loop() {
 
+  // Create variables to handle button states
   int reading_ok = digitalRead(btn_ok);
   int reading_left = digitalRead(btn_left);
   int reading_right = digitalRead(btn_right);
   
   unsigned long currentMillis = millis();
 
+  // Update debounce and add delay to auto-close menu
   if (reading_ok != lastButtonState_ok) {
     lastDebounceTime = millis();
     del_menu = millis();
@@ -137,30 +139,31 @@ void loop() {
     del_menu = millis();
   }
 
+  // Debounce check
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading_ok == LOW && !btnPressed_ok) {
+    if (reading_ok == LOW && !btnPressed_ok) {          // OK button pressed check
       digitalWrite(led,HIGH);
-      btnPressed_ok = true;  				// Кнопка натиснута
+      btnPressed_ok = true;  				                    // Mark button as pressed
       
-      if (!menuVisible){
-        menuVisible = true;
+      if (!menuVisible){                                // Show menu if not visible
+        menuVisible = true; 
         drawMenu();
-      } else if (!inSubmenu){
+      } else if (!inSubmenu){                           // If in main menu, start from first submenu
         submenuIndex = 0;
-        if(menuIndex == 2){
+        if(menuIndex == 2){                             // Toggle pause mode
           isPaused = !isPaused;
           inSubmenu = false;
           drawMenu();
-        }else {
+        }else {                                         // Enter submenu
         inSubmenu = true;
         drawSubmenu ();
         }
       } else {
-        if (menuIndex == 0){
+        if (menuIndex == 0){                            // Submenu switching (color palette)
           currentColourMode = submenuIndex;
           inSubmenu = false;
           drawMenu();
-        }else if(menuIndex == 1){
+        }else if(menuIndex == 1){                       // Selecting temperature info mode and clearing bottom section
           currentTempMode = submenuIndex;
           tft.fillRect(74, 128, 128, 32, ST7735_BLACK);
           tft.fillRect(0, 128, 128, 32, ST7735_BLACK);
@@ -171,25 +174,25 @@ void loop() {
 
     }
 
-    if (currentMillis - del_menu >= 5000){
+    if (currentMillis - del_menu >= 5000){               // 5-second inactivity clears display
       tft.fillRect(0, 128, 74, 8, ST7735_BLACK);
     }
     digitalWrite(led,LOW);
   }
   
-  lastButtonState_ok = reading_ok;
+  lastButtonState_ok = reading_ok;                       // Update OK button state
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay) {   // LEFT button debounce check
     if (reading_left == LOW && !btnPressed_left) {
       btnPressed_left = true;
       digitalWrite(led,HIGH);
       
       if (menuVisible){
-        if (!inSubmenu){
+        if (!inSubmenu){                                 // Main menu navigation left
           menuIndex = (menuIndex - 1 + mainMenuSize) % mainMenuSize;
           drawMenu();
-        }else {
-          if (menuIndex == 0){
+        }else {                                          // Submenu navigation left
+          if (menuIndex == 0){    
             submenuIndex = (submenuIndex - 1 + colourModesSizes) % colourModesSizes;
             drawSubmenu ();
           }else if (menuIndex == 1){
@@ -200,7 +203,7 @@ void loop() {
       }
     }
 
-    if (currentMillis - del_menu >= 5000 && menuVisible){
+    if (currentMillis - del_menu >= 5000 && menuVisible){ // 5-second inactivity clears display
       tft.fillRect(0, 128, 74, 8, ST7735_BLACK);
       menuVisible = false;
       inSubmenu = false;
@@ -209,18 +212,18 @@ void loop() {
     digitalWrite(led,LOW);
   }
 
-  lastButtonState_left = reading_left;
+  lastButtonState_left = reading_left;                    // Update LEFT button state
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay) {    // RIGHT button debounce check
     if (reading_right == LOW && !btnPressed_right) {
       btnPressed_right = true;
       digitalWrite(led,HIGH);
 
       if (menuVisible){
-        if (!inSubmenu){
+        if (!inSubmenu){                                  // Main menu navigation right
           menuIndex = (menuIndex + 1) % mainMenuSize;
           drawMenu();
-        }else {
+        }else {                                           // Submenu navigation right
           if (menuIndex == 0){
             submenuIndex = (submenuIndex + 1) % colourModesSizes;
             drawSubmenu ();
@@ -232,7 +235,7 @@ void loop() {
       }
     }
 
-    if (currentMillis - del_menu >= 5000 && menuVisible){
+    if (currentMillis - del_menu >= 5000 && menuVisible){ // 5-second inactivity clears display
       tft.fillRect(0, 128, 74, 8, ST7735_BLACK);
       menuVisible = false;
       inSubmenu = false;
@@ -241,26 +244,29 @@ void loop() {
     digitalWrite(led,LOW);
   }
 
-  lastButtonState_right = reading_right;
+  lastButtonState_right = reading_right;                  // Update RIGHT button state
 
-  if (!isPaused && currentMillis - lastUpdate >= updateInterval) { // оновлення кожні 100 мс
-    lastUpdate = currentMillis;
+  if (!isPaused && currentMillis - lastUpdate >= updateInterval) { 
+    lastUpdate = currentMillis;                           // Update every 100 ms
 
-    amg.readPixels(pixels); // Зчитування масиву температур 
+    amg.readPixels(pixels);                               // Read temperature array from sensor
 
+    // Copy data to array for sorting
     float sortedPixels [64];
     memcpy(sortedPixels, pixels, sizeof(pixels));
 
-    bubbleSort(sortedPixels, 64);
+    bubbleSort(sortedPixels, 64);                         // Call function and pass data
 
+    // Determine quantiles
     float qLow = getQuantile(sortedPixels, 64, 0.2);
     float qHigh = getQuantile(sortedPixels, 64, 0.95);
 
+    // Raw sensor temperature data
     float rawMinT = 999;
     float rawMaxT = -999;
     float sumT = 0;
 
-    float centerT = pixels[4 * gridSize + 4];
+    float centerT = pixels[4 * gridSize + 4];             // Center pixel
 
     for (int i = 0; i < 64; i++){
       sumT += pixels[i];
@@ -278,6 +284,7 @@ void loop() {
     float desiredMin = 0;
     float desiredMax = 0;
 
+    // Calculate desired temperature range for display
     if (range < minWindowSize){
       desiredMin = antiNoise + avgT - minWindowSize / 2.0;
       desiredMax = avgT + minWindowSize / 2.0;
@@ -289,9 +296,11 @@ void loop() {
       desiredMax = qHigh;
     }
 
+    // Smooth update of temperature limits
     tMin = 0.9 * tMin + 0.1 * desiredMin;
     tMax = 0.9 * tMax + 0.1 * desiredMax;
 
+    // Draw temperature map to the screen
     for (int y = 0; y < gridSize; y++) {
       for (int x = 0; x < gridSize; x++) {
         int i = (gridSize - 1 - y) * gridSize + (gridSize - 1 - x);
@@ -308,10 +317,11 @@ void loop() {
       }
     }
     
-    drawTemp(rawMinT, rawMaxT, avgT, centerT);
+    drawTemp(rawMinT, rawMaxT, avgT, centerT);            // Call function to show temperature values
 
   }
 
+  // Reset button state
   if (reading_ok == HIGH) {
     btnPressed_ok = false;
   }
@@ -324,6 +334,7 @@ void loop() {
   
 }
 
+// Sort temp. array
 void bubbleSort (float arr[], int n) {
   for (int i = 0; i < n-1; i++){
     for (int j = 0; j < n-i-1; j++){
@@ -336,14 +347,16 @@ void bubbleSort (float arr[], int n) {
   }
 }
 
+// Choising and returning quantile  
 float getQuantile (float arr[],int n, float q){
   int index = (int)(q*(n-1));
   return arr[index];
 }
 
+// Data processing and transmission to the display as colour pallette
 uint16_t mapTemperatureToColor(float temp, float tMin, float tMax) {
 
-  // Обмеження діапазону
+  // Range limitation
   if (temp < tMin) temp = tMin;
   if (temp > tMax) temp = tMax;
 
@@ -351,16 +364,16 @@ uint16_t mapTemperatureToColor(float temp, float tMin, float tMax) {
 
   switch(currentColourMode) {
     case 0:
-      return tft.color565(value, value, value); // Червоне – тепле
+      return tft.color565(value, value, value); // Red – hot
       break;
     case 1:
-      return tft.color565(255 - value, 255 - value, 255 - value); // Біле – тепле
+      return tft.color565(255 - value, 255 - value, 255 - value); // White – hot
       break; 
     case 2:
-      return tft.color565(value, value/4, 0); //  Червоне – тепле
+      return tft.color565(value, value/4, 0); //  Red – hot, black – cold
       break;
     case 3:
-      return tft.color565(value, 0, 255 - value); // Червоне – тепле, синє холодне
+      return tft.color565(value, 0, 255 - value); // Red – hot, blue – cold
       break; 
     case 4:
       float f = value / 255.0f;
@@ -408,7 +421,7 @@ void drawTemp (float rawMinT, float rawMaxT, float avgT, float centerT){
   tft.setTextColor(ST7735_WHITE);
 
     if (currentTempMode == 0) {
-    // Clear: нічого не малюємо
+    // Clear: empty dysplay
   }
 
   if (currentTempMode == 1) {
